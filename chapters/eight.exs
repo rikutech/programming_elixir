addd0af18c8085d2f7fa3e599384159589586eea
rikutech <rikutech@gmail.com>
2018-07-18 09:39:16 +0900

    nested struct

--- chapters/eight.exs
+++ chapters/eight.exs
@@ -1,3 +1,11 @@
 defmodule Subscriber do
   defstruct name: "", paid: false, over_18: true
 end
+
+defmodule Customer do
+  defstruct name: "", company: ""
+end
+
+defmodule BugReport do
+  defstruct owner: %Customer{}, details: "", severity: 1
+end
