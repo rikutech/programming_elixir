6be76bdb2c0c5730c7e78b1da702e420dd16d2a3
rikutech <rikutech@gmail.com>
2018-07-18 21:52:37 +0900

    chapter 8 finished

--- chapters/eight.exs
+++ chapters/eight.exs
@@ -9,3 +9,21 @@ end
 defmodule BugReport do
   defstruct owner: %Customer{}, details: "", severity: 1
 end
+
+defmodule Main do
+  def main do
+    report = %BugReport{owner: %Customer{name: "Dave", company: "Pragmatic"}, details: "broken"}
+    IO.inspect report
+    IO.inspect put_in(report.owner.company, "PragProg")
+  end
+
+  def map_set do
+    set1 = Enum.into 1..5, MapSet.new
+    MapSet.member? set1, 3 # => true
+    set2 = Enum.into 3..8, MapSet.new
+    MapSet.union set1, set2
+    MapSet.difference set1, set2
+    MapSet
+  end
+end
+
