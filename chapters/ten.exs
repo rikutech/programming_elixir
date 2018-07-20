79d8b342b0b82ee189c1afe1b02f1684b08d9b32
rikutech <rikutech@gmail.com>
2018-07-20 23:52:38 +0900

    stream

--- chapters/ten.exs
+++ chapters/ten.exs
@@ -39,3 +39,23 @@ defmodule Main do
     IO.inspect Enum.join(list)
   end
 end
+
+
+#Stream 遅延列挙
+odds = [1,2,3,4]
+|> Stream.map(&(&1 * &1))
+|> Stream.map(&(&1+1))
+|> Stream.filter(fn x -> rem(x,2) == 1 end)
+#Enumモジュールの関数を使って初めて評価される
+IO.inspect Enum.to_list odds
+
+Stream.map(1..10_000_000, &(&1+1)) |> Enum.take(5)
+
+#新しい値が必要になったときに都度実行される
+IO.inspect Stream.repeatedly(fn  -> true end) |> Enum.take(3)
+IO.inspect Stream.repeatedly(fn  -> true end) |> Enum.take(5)
+
+#無限ストリームの生成 初期値と適用関数を渡す　結果が次の初期値になる　ストリームが使われている限り続ける
+IO.inspect Stream.iterate(0, &(&1+1)) |> Enum.take(5)
+#フィボナッチ数列の実装 初期値のタプルと関数を渡すと、適用されて1つずつずれていく
+IO.inspect Stream.unfold({0, 1}, fn {f1, f2} -> {f1, {f2, f1+f2}} end) |> Enum.take(15)
