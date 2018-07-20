4b71d3c5da2098e73e5c7029ec67e6e04a0b6f33
rikutech <rikutech@gmail.com>
2018-07-20 09:15:14 +0900

    WIP

--- chapters/ten.exs
+++ chapters/ten.exs
@@ -1,3 +1,34 @@
+defmodule MyEnum do
+  def all?([head|tail], func) do
+    if func.(head), do: all?(tail, func), else: false
+  end
+  def all?([], _), do: true
+
+  def each([head|tail], func) do
+    func.(head)
+    each(tail, func)
+  end
+  def each([], _), do: true
+
+  def filter([head|tail], func) do
+    if func.(head), do: [head|filter(tail, func)], else: filter(tail, func)
+  end
+  def filter([], _), do: []
+
+  def flatten(all = [head|tail]) when is_list(head) do
+    IO.puts "is_list"
+    IO.inspect all
+    [flatten(tail)|flatten(head)]
+  end
+  def flatten(all = [head|tail]) do
+    IO.puts "is_single"
+    IO.inspect all
+    [flatten(tail)|[head]]
+  end
+  def flatten([]), do: []
+end
+
+
 defmodule Main do
   def main do
     list = Enum.to_list 1..5
